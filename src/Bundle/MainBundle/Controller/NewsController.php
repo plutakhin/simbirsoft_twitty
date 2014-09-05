5027560a1f503409d6a51087d058aa8279a046f6
Pavel Plutakhin <pavel.plutakhin@simbirsoft.com>
2014-09-05 13:43:40 +0400

    news list

--- src/Bundle/MainBundle/Controller/NewsController.php
+++ src/Bundle/MainBundle/Controller/NewsController.php
@@ -2,15 +2,31 @@
 
 namespace Bundle\MainBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\HttpFoundation\Request;
 use Bundle\MainBundle\Controller\BaseController;
 
 /**
  * @TODO Реализовать работу с новостями
+ * @Route("/news")
  */
 class NewsController extends BaseController
 {
+    /**
+     * @Route("/", name="news_list")
+     * @Method("GET")
+     * @Template()
+     */
+    public function listAction(Request $request)
+    {
+        $tag = $request->get('tag');
+        $news = $this->getRepository('News')->findFor($this->getUser(), $tag);
+
+        return array(
+            'news' => $news,
+        );
+    }
 }
--- src/Bundle/MainBundle/Repository/NewsRepository.php
+++ src/Bundle/MainBundle/Repository/NewsRepository.php
@@ -7,12 +7,31 @@ use Bundle\MainBundle\Entity\News;
 use Bundle\MainBundle\Repository\BaseRepository;
 use Doctrine\ORM\Query;
 
+use Bundle\MainBundle\Entity\User;
+
 /**
  * NewsRepository
  */
 class NewsRepository extends BaseRepository
 {
-    /**
-     * @TODO
-     */
+    public function findFor(User $user, $tag = null)
+    {
+        $query = $this->createQueryBuilder('n');
+        $query
+            ->where('n.author in (:subs)')
+            ->setParameter('subs', $user->getSubscriptions()->toArray())
+            ->setMaxResults(100)
+            ->orderBy('n.id', 'DESC')
+            ;
+
+        if ($tag) {
+            $query
+                ->innerJoin('n.tags', 't')
+                ->andWhere('t.name = :name')
+                ->setParameter('name', $tag)
+                ;
+        }
+
+        return $query->getQuery()->getResult();
+    }
 }
